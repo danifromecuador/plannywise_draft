b0096ccb269be7db98e44e498b65b62d2a7e6a1a
dani <danifromecuador@gmail.com>
2024-04-10 12:39:27 -0500

    Create a test store using Zustand

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,8 +1,11 @@
+import { store } from '../store/store.js'
 import './App.css'
+
 export const App = () => {
   return (
     <div className="app">
       Hello Dani
+      {store.getState().bears}
     </div>
   )
 }
