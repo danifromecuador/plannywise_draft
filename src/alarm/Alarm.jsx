d0c68d43f23268aacfd996621afc1fd7fa748233
dani <danifromecuador@gmail.com>
2024-04-10 13:16:55 -0500

    Create and Style layout of Alarm component

--- src/alarm/Alarm.jsx
+++ src/alarm/Alarm.jsx
@@ -7,7 +7,18 @@ export const Alarm = () => {
   const alarmStore = AlarmStore.getState()
   return (
     <div className="alarm">
-      Alarm Interval : {alarmStore.interval}
+      <div className="alarm-left">
+        <span>Alarm is ON </span>
+        <span>Click to turn off</span>
+      </div>
+      <div className="alarm-center">
+        <span>Alarm will sound </span>
+        <span>every <span>15</span> minutes</span>
+      </div>
+      <div className="alarm-right">
+        <span>Next alarm will sound </span>
+        <span>at <span>15:30</span></span>
+      </div>
     </div>
   )
 }
