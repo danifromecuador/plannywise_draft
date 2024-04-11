365acaf4d11655f0ca42284eafc36564479d4652
dani <danifromecuador@gmail.com>
2024-04-11 12:09:23 -0500

    Style Todo component

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -14,27 +14,30 @@ export const Todo = () => {
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todoStore.todos))
     localStorage.setItem("dones", JSON.stringify(todoStore.dones))
-  },[todoStore])
+  }, [todoStore])
 
   return (
     <div className="todo">
-      <ul>
+      <h1>Daily Goals</h1>
+      <ul className={todoStore.todos.length === 0 && todoStore.dones.length === 0 ? "hide" : ""}>
         {todoStore.todos.map((t) => (<li key={t.index} className='todo-todo' onClick={() => todoStore.markAsDone(t)}>{t.text} </li>))}
         {todoStore.dones.map((d) => (<li key={d.index} className='todo-done' onClick={() => todoStore.markAsUnDone(d)}>{d.text}</li>))}
       </ul>
-      <button onClick={() => todoStore.deleteAllCompleted()}>
-        Delete All Completed
-      </button>
-      <input
-        type="text"
-        placeholder='Type a new goal and press Enter'
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        onKeyDown={(e) => handleEnterKey(e)}
-      />
+      <div className="button-and-input">
+        <button onClick={() => todoStore.deleteAllCompleted()}>
+          Delete All Completed
+        </button>
+        <input
+          type="text"
+          placeholder='Type a new goal and press Enter'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => handleEnterKey(e)}
+        />
+      </div>
     </div>
   )
 }
