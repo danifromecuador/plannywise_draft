8c3bf9873c4f3aab613b283d69040dc09664f1b2
dani <danifromecuador@gmail.com>
2024-04-10 19:18:24 -0500

    Conditional rendering of aditional info in Alarm component

--- src/alarm/Alarm.jsx
+++ src/alarm/Alarm.jsx
@@ -1,21 +1,23 @@
 // src/alarm/Alarm.jsx
 
+import { useState } from 'react'
 import { AlarmStore } from '../store/store.js'
 import './Alarm.css'
 
 export const Alarm = () => {
   const { alarmState, alarmMessage, changeAlarmState } = AlarmStore()
+
   return (
     <div className="alarm">
       <div className="alarm-left" onClick={changeAlarmState}>
         <span>{alarmState} </span>
         <span>{alarmMessage}</span>
       </div>
-      <div className="alarm-center">
+      <div className={`alarm-center ${alarmState === "Alarm is OFF" ? "hide" : ""}`}>
         <span>Alarm will sound </span>
         <span>every <span>15</span> minutes</span>
       </div>
-      <div className="alarm-right">
+      <div className={`alarm-right ${alarmState === "Alarm is OFF" ? "hide" : ""}`}>
         <span>Next alarm will sound </span>
         <span>at <span>15:30</span></span>
       </div>
