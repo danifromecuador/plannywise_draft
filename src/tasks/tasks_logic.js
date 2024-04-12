6f1851a3762cde1e2dace714c3fea1bb9a0ec414
dani <danifromecuador@gmail.com>
2024-04-12 08:42:42 -0500

    Add an unique index based on date and time to each completed task

--- src/tasks/tasks_logic.js
+++ src/tasks/tasks_logic.js
@@ -1,16 +1,17 @@
 // src/tasks/tasks_logic.js
 
-import { previousInterval } from "../alarm/alarm_logic";
+import { getIndex, previousInterval } from "../alarm/alarm_logic";
 import { TaskStore } from "../store/store"
 
 export const addCompletedTask = (set, input) => {
   const tasks = TaskStore.getState().completedTasks
   if (tasks.length === 0) {
-    set((state) => ({ completedTasks: [...state.completedTasks, { interval: previousInterval(), text: input }] }))
+    set((state) => ({ completedTasks: [...state.completedTasks, { index: getIndex(), interval: previousInterval(), text: input }] }))    
   }
   if (tasks.length > 0 && tasks[tasks.length - 1].interval !== previousInterval()) {
-    set((state) => ({ completedTasks: [...state.completedTasks, { interval: previousInterval(), text: input }] }))
+    set((state) => ({ completedTasks: [...state.completedTasks, { index: getIndex(), interval: previousInterval(), text: input }] }))    
   }
+  console.log(tasks[tasks.length-1]);
 }
 
 export const workedHours = () => TaskStore.getState().completedTasks.length / 4
