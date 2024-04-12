c443f1b58b913d7a21a30040ed29c8d88fca8e8b
dani <danifromecuador@gmail.com>
2024-04-12 10:02:55 -0500

    Add progress indicator and Spread/Hide button on Goals component

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -7,6 +7,10 @@ import './Todo.css'
 export const Todo = () => {
   const todoStore = TodoStore()
   const [input, setInput] = useState("")
+  const [collapseClass, setCollapseClass] = useState("")
+  const [hide1, setHide1] = useState("")
+  const [hide2, setHide2] = useState("hide")
+
   const handleEnterKey = (e) => {
     if (e.key === "Enter" && input && input[0] !== " ") {
       todoStore.addTodo(input)
@@ -19,14 +23,35 @@ export const Todo = () => {
     localStorage.setItem("dones", JSON.stringify(todoStore.dones))
   }, [todoStore])
 
+  const calculateCompletedPercentage = () => Math.floor((todoStore.dones.length / (todoStore.todos.length + todoStore.dones.length)) * 100)
+
+  const collapse = () => {
+    if (collapseClass === "") {
+      setCollapseClass("hide")
+      setHide1("hide")
+      setHide2("")
+    }
+    else {
+      setCollapseClass("")
+      setHide1("")
+      setHide2("hide")
+    }
+  }
+
   return (
     <div className="todo">
-      <h1>Daily Goals</h1>
-      <ul className={todoStore.todos.length === 0 && todoStore.dones.length === 0 ? "hide" : ""}>
+      <div className="header">
+        <span className={isNaN(calculateCompletedPercentage()) ? "hide" : "todo-completed"}>Completed: <span className='todo-completed-number'>{calculateCompletedPercentage()}%</span></span>
+        <h1>Daily Goals</h1>
+        {/* <div className='todo-collapse-up' onClick={collapse}></div> */}
+        <div className={`todo-collapse-up ${hide1}`} onClick={collapse}></div>
+        <div className={`todo-collapse-down ${hide2}`}  onClick={collapse}></div>
+      </div>
+      <ul className={` ${collapseClass} ${todoStore.todos.length === 0 && todoStore.dones.length === 0 ? "hide" : ""}`}>
         {todoStore.todos.map((t) => (<li key={t.index} className='todo-todo' onClick={() => todoStore.markAsDone(t)}>{t.text} </li>))}
         {todoStore.dones.map((d) => (<li key={d.index} className='todo-done' onClick={() => todoStore.markAsUnDone(d)}>{d.text}</li>))}
       </ul>
-      <div className="button-and-input">
+      <div className={collapseClass === "" ? "button-and-input" : "hide"}>
         <button onClick={() => todoStore.deleteAllCompleted()}>
           Delete All Completed
         </button>
@@ -38,6 +63,6 @@ export const Todo = () => {
           onKeyDown={(e) => handleEnterKey(e)}
         />
       </div>
-    </div>
+    </div >
   )
 }
