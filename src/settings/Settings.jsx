66006a8e3bea6b5dbc23bd0a4f5c0dc13b8520e2
dani <danifromecuador@gmail.com>
2024-04-16 12:52:09 -0500

    Create a pop up or modal window

--- src/settings/Settings.jsx
+++ src/settings/Settings.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react'
 import './Settings.css'
 
 export const Settings = () => {
+  const [hide1, setHide1] = useState("")
+  const [hide2, setHide2] = useState("hide")
+  const handleHide = () => {
+    if (hide1 === "") {
+      setHide1("hide")
+      setHide2("")
+    } else {
+      setHide1("")
+      setHide2("hide")
+    }
+  }
+
   return (
     <div className="settings">
-      <div className="icon">
+      <div className={`icon ${hide1}`} onClick={handleHide}>
         ?
       </div>
-      <div className="modal hide">
-        a lot of things
+      <div className={`modal ${hide2}`}>
+        <button onClick={handleHide}>X</button>
+        <div>a lot of things in this div</div>
       </div>
     </div>
   )
